b7ed80c1a7bb689e1084679cfb2bcb02147c7d39
mc2266 <55021131+michaeljjacob@users.noreply.github.com>
2024-02-22 13:34:00 -0800

    updated tests

--- test/zebra_scanner_plugin_test.dart
+++ test/zebra_scanner_plugin_test.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:zebra_scanner_plugin/src/zebra_scanner_plugin.dart';
+import 'package:zebra_scanner_plugin/zebra_scanner_plugin.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('zebra_scanner_plugin');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  const channelName = 'zebra_scanner_plugin';
+  const MethodChannel channel = MethodChannel(channelName);
+
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    // Intercept the method channel call and provide a mock response
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      channel,
+      (MethodCall methodCall) async {
+        if (methodCall.method == 'getPlatformVersion') {
+          return '42';
+        }
+        return null;
+      },
+    );
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    // Remove the mock method call handler
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      channel,
+      null,
+    );
   });
 
   test('getPlatformVersion', () async {
